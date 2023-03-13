f3d6fe4c2efb12db9d8ca0848518af2f81cd4e84
chenqifeng66 <839860616@qq.com>
2023-03-13 18:59:19 +0800

    聊天页消息搭建

--- pages/chat/Chat.vue
+++ pages/chat/Chat.vue
@@ -1,5 +1,22 @@
 <template>
 	<view class="container">
+		<!-- 聊天内容区域 -->
+		<view class="main">
+			<view class="message">
+				<image src="../../static/my-icons/fox.png" mode="scaleToFill"></image>
+				<view class="content">
+					<text>这是一条消息这是一条消息这是一条消息这是一条消息这是一条消息这是一条这是一条消息这是一条消息这是一条</text>
+				</view>
+			</view>
+			<view class="message right">
+				<image src="../../static/my-icons/rabbit.png" mode="scaleToFill"></image>
+				<view class="content">
+					<text>这是一条消息这是一条消息这是一条消息</text>
+				</view>
+			</view>
+		</view>
+		
+		<!-- 底部控件 -->
 		<view class="bottom">
 			<view class="left">
 				<image src="../../static/my-icons/语音.png" mode="scaleToFill"></image>
@@ -22,6 +39,8 @@
 .container{
 	height: 100vh;
 	background-color: #ccc;
+	
+	// 底部控件
 	.bottom{
 		height: 100rpx;
 		background-color: #fff;
@@ -64,6 +83,40 @@
 			}
 		}
 		
+	}
+
+	// 聊天内容区域
+	.main{
+		height: 100%;
+		margin-bottom: 100rpx;
+		padding: 0 20rpx;
+		
+		.message{
+			display: flex;
+			align-items: flex-start;
+			padding: 15rpx 0;
+			
+			image{
+				height: 50rpx;
+				width: 50rpx;
+			}
+			
+			.content{
+				background-color: #fff;
+				margin-left: 10rpx;
+				padding:10rpx;
+				border-radius: 8rpx;
+				max-width: 70%;
+			}
+		}
+		
+		.right{
+			flex-direction: row-reverse;
+			
+			.content{
+				margin-right: 10rpx;
+			}
+		}
 	}
 }
 </style>
