5a1a99ab180d2ea671f5dd7263b97b88dad47c79
chenqifeng66 <839860616@qq.com>
2023-03-13 18:20:01 +0800

    聊天页面底部

--- pages/chat/Chat.vue
+++ pages/chat/Chat.vue
@@ -1,6 +1,17 @@
 <template>
 	<view class="container">
-		
+		<view class="bottom">
+			<view class="left">
+				<image src="../../static/my-icons/语音.png" mode="scaleToFill"></image>
+			</view>
+			<view class="middle">
+				<input type="text">
+			</view>
+			<view class="right">
+				<image src="../../static/my-icons/表情.png" mode="scaleToFill"></image>
+				<image src="../../static/my-icons/加号.png" mode="scaleToFill"></image>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,5 +19,51 @@
 </script>
 
 <style lang="scss" scoped>
-
+.container{
+	height: 100vh;
+	background-color: #ccc;
+	.bottom{
+		height: 100rpx;
+		background-color: #fff;
+		position: fixed;
+		bottom: 0;
+		width: 100%;
+		display: flex;
+		align-items: center;
+		
+		.left{
+			width: 10%;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			image{
+				height: 50rpx;
+				width: 50rpx;
+			}
+		}
+		
+		.middle{
+			width: 75%;
+			box-sizing: border-box;
+			margin-right: 5rpx;
+			input{
+				border-radius: 5px;
+				height: 60rpx;
+				background-color: #ccc;
+			}
+		}
+		
+		.right{
+			width: 15%;
+			display: flex;
+			justify-content: space-around;
+			margin-right: 5rpx;
+			image{
+				height: 50rpx;
+				width: 50rpx;
+			}
+		}
+		
+	}
+}
 </style>
--- unpackage/dist/dev/mp-weixin/pages/chat/Chat.js
+++ unpackage/dist/dev/mp-weixin/pages/chat/Chat.js
@@ -4,5 +4,5 @@ const _sfc_main = {};
 function _sfc_render(_ctx, _cache) {
   return {};
 }
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "/Users/lindo001/work/bishe/pages/chat/Chat.vue"]]);
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-17e92379"], ["__file", "/Users/lindo001/work/bishe/pages/chat/Chat.vue"]]);
 wx.createPage(MiniProgramPage);
